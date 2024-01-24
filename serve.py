b77bfec21ed0e769d423a1c0cdf99356d33416dc
kdural2 <55032182+kdural2@users.noreply.github.com>
2024-01-23 19:25:47 -0600

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import socket
+
+app = Flask(__name__)
+
+data = {'num':0}
+
+@app.route('/', methods = ['POST', 'GET'])
+def hello_world():
+   global data
+   if (request.method == 'GET'):
+      return socket.gethostname()
+   if (request.method == 'POST'):
+      args = ['python3', 'stress_cpu.py']
+      proc = subprocess.Popen(args)
+      return 'Success'
+
+
+if __name__ == "__main__":
+ app.run(host = "0.0.0.0")
